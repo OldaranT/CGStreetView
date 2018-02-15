49a8d431d29ecb4230ff4a09518852a90d5bee86
OldaranT <timbaak@hotmail.com>
2018-02-15 11:57:08 +0100

    Added semi car

--- js/obj/House1.js
+++ js/obj/House1.js
@@ -2,7 +2,6 @@
 
 function LoadPillar(x, y, z){
 
-
     TextureLoader.load('images/Pillar/PillarTexture.jpg', function ( texture ) {
         var BaseLantern = new THREE.SphereGeometry(0.055, 32, 32);
         var PillarMaterial = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
@@ -185,11 +184,39 @@ function LoadOuterSideWalk(x, y, z) {
     });
 }
 
+function ReturnCar(x, y, z){
+
+    var Car = new THREE.Object3D();
+
+    TextureLoader.load('images/Pillar/PillarTexture.jpg', function ( texture ) {
+        var BaseLantern = new THREE.BoxGeometry(0.055, 32, 32);
+        var PillarMaterial = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
+        var RingLantern = new THREE.TorusGeometry(0.06, 0.015, 30, 200);
+        var GroundLantern = new THREE.CylinderGeometry(0.05, 0.15, 1, 500);
+        // Optimizing quality of texture.
+        texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+        var BaseMesh = new THREE.Mesh(BaseLantern, PillarMaterial);
+        var GroundMesh = new THREE.Mesh(GroundLantern, PillarMaterial);
+        var RingMesh = new THREE.Mesh(RingLantern, PillarMaterial);
+        BaseMesh.position.set(x, y + 0.5, z);
+        GroundMesh.position.set(x, y, z);
+        RingMesh.position.set(x, y + 0.5, z);
+        RingMesh.rotateX(-Math.PI / 2);
+
+        Car.add(BaseMesh);
+        Car.add(RingMesh);
+        Car.add(GroundMesh);
+    });
+    return Car;
+}
+
 var length = 5;
 
 for(var i = 0; i < 2*Math.PI; i += 1/16*Math.PI){
     LoadPillar(length*Math.cos(i),1,length*Math.sin(i));
 }
+
+
 LoadPillar(0,1,0);
 LoadRoad(0,0.5,0);
 LoadOuterSideWalk(0,0.5,0);
---
+++ scene.js
@@ -0,0 +1,8 @@
+
+// Create scene
+var scene = new THREE.Scene();
+
+
+
+//Create Loader
+var TextureLoader = new THREE.TextureLoader();
